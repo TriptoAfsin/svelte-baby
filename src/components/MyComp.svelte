22a9482a887e87c02699a119c978c1cadd258eb9
TriptoAfsin <afsintripto@gmail.com>
2023-08-22 10:30:24 +0600

    chore: initial commit

---
+++ src/components/MyComp.svelte
@@ -0,0 +1,59 @@
+<script>
+    import { spring} from 'svelte/motion';
+	export let myValue = 'default value'
+    export let myString = 'default string'
+    let myHtml = `<b>Hello custom html</b>`;
+    let count = 0
+    let inputText = 'Type Below 😁'
+    let handleTextChange = (e) => {
+        console.log('called me')
+        inputText = e.target.value
+    }
+    let myArrObj = [
+        {
+            id: 1,
+            name: 'Tripto'
+        },
+        {
+            id: 2,
+            name: 'Alina'
+        },
+        {
+            id: 3,
+            name: 'Juni'
+        }
+    ]
+    const displayed_value = spring();
+    $: displayed_value.set(myValue)
+    
+</script>
+
+<div class="flex-column my-container">
+	My Own Svelte Component
+   <p>Custom Value: {myValue}</p> 
+   <p>Custom String: {myString}</p> 
+   <p>Pi: {Math.PI}</p> 
+   <p>Random Num: {Math.floor(Math.random()*10)}</p> 
+   <ul>{#each myArrObj  as item}
+    <li>{item.name}</li>
+   {/each}</ul> 
+   <p>Count: {count}</p>
+   <button type="button" on:click={() => count++}>+</button>
+   <button type="button" on:click={() => count--}>-</button>
+   <p>{inputText}</p>
+   <input placeholder="type here" on:change={handleTextChange} class="px-2 py-2">
+   <div class="mt-3">{@html myHtml}</div>
+</div>
+
+<style>
+    .my-container{
+        border: 1px solid greenyellow;
+        padding: 10px;
+    }
+	.flex-column{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,49 @@
+<script>
+	import Counter from '../components/Counter.svelte';
+	import welcome from '$lib/images/svelte-welcome.webp';
+	import welcome_fallback from '$lib/images/svelte-welcome.png';
+	import MyComp from '../components/MyComp.svelte';
+	import TailCard from '../components/TailCard.svelte';
+</script>
+
+<svelte:head>
+	<title>Svelte baby app</title>
+	<meta name="description" content="Svelte baby app" />
+</svelte:head>
+
+<section>
+
+	<Counter />
+	<MyComp myString={'Passed from prop'} myValue={120}/>
+	<TailCard />
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+
+	h1 {
+		width: 100%;
+	}
+
+	.welcome {
+		display: block;
+		position: relative;
+		width: 100%;
+		height: 0;
+		padding: 0 0 calc(100% * 495 / 2048) 0;
+	}
+
+	.welcome img {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		top: 0;
+		display: block;
+	}
+</style>
